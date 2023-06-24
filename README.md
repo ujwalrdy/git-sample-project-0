d194421d96f58af1f22d44bcbf5560811c0a9b4f
greymtr <greymtr@disroot.org>
2023-06-24 06:30:22 +0530

    Added Files

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # git-sample-project-0
 Sample Project for Git events
+
+Sample Project 0 is a simple calculator.
+calculator.py contains the program.
+
+Fix the errors, and submit a Pull Request.
---
+++ calculator.py
@@ -0,0 +1,23 @@
+#
+# calculator.py 
+#
+
+# Asks user for 2 operands and 1 operator
+# Returns output of this operation
+
+a=input("Enter number 1 : ")
+o=input("Enter operator : ")
+b=input("Enter number 2 : ")
+
+if o[0] in [ '+','-','*','/' ]:
+    if o[0] == '+':
+        out = a + b
+    elif o[0] == '-':
+        out = a - b
+    elif o[0] == '*':
+        out = a * b
+    elif o[0] == '/':
+        out = a//b
+    print("Output : ",out)
+else:
+    print("Error : Invalid Operator")
